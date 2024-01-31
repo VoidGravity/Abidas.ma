077c2e58e2e6697c0657caf27a26959b9c16197d
VoidGravity <abdellahbardichwork@gmal.com>
2024-01-31 11:36:30 +0100

    the class diagrame

---
+++ example-app/resources/views/sidebar.blade.php
@@ -0,0 +1,60 @@
+<aside class="sidebar position-fixed top-0 left-0 overflow-auto h-100 float-left" id="show-side-navigation1">
+        
+        <div class="sidebar-header d-flex justify-content-center align-items-center px-3 py-4">
+            <img class="rounded-pill img-fluid" width="65" src="https://uniim1.shutterfly.com/ng/services/mediarender/THISLIFE/021036514417/media/23148907008/medium/1501685726/enhance" alt="">
+            <div class="ms-2">
+                <h5 class="fs-6 mb-0">
+                    <a class="text-decoration-none" href="#">Jone Doe</a>
+                </h5>
+                <p class="mt-1 mb-0">Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+        </div>
+
+        <div class="search position-relative text-center px-4 py-3 mt-2">
+            <input type="text" class="form-control w-100 border-0 bg-transparent" placeholder="Search here">
+        </div>
+
+        <ul class="categories list-unstyled">
+            <li >
+                <a href="#"> Dashboard</a>
+                
+            </li>
+            <li >
+                <a href="#"> File manager</a>
+            </li>
+            <li  >
+                
+            <a href="#"> Calender</a>
+                <ul class="sidebar-dropdown list-unstyled">
+                    <li><a href="#">Lorem ipsum</a></li>
+                    <li><a href="#">ipsum dolor</a></li>
+                    <li><a href="#">dolor ipsum</a></li>
+                    <li><a href="#">amet consectetur</a></li>
+                    <li><a href="#">ipsum dolor sit</a></li>
+                </ul>
+            </li>
+            
+            <li class="">
+               <a href="#"> Settings</a>
+                <ul class="sidebar-dropdown list-unstyled">
+                    <li><a href="#">Lorem ipsum</a></li>
+                    <li><a href="#">ipsum dolor</a></li>
+                    <li><a href="#">dolor ipsum</a></li>
+                    <li><a href="#">amet consectetur</a></li>
+                    <li><a href="#">ipsum dolor sit</a></li>
+                </ul>
+            </li>
+            <li  >
+                <a href="#"> Components</a>
+                <ul class="sidebar-dropdown list-unstyled">
+                    <li><a href="#">Lorem ipsum</a></li>
+                    <li><a href="#">ipsum dolor</a></li>
+                    <li><a href="#">dolor ipsum</a></li>
+                    <li><a href="#">amet consectetur</a></li>
+                    <li><a href="#">ipsum dolor sit</a></li>
+                </ul>
+            </li>
+            
+            
+        </ul>
+    </aside>
